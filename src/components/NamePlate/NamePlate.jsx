7f8774fa060d8ed1ca329193b8e163ff8f65bffc
shahzahandev <shahzahansiraj516@gmail.com>
2025-09-29 19:21:52 +0600

    sloved img problem

--- src/components/NamePlate/NamePlate.jsx
+++ src/components/NamePlate/NamePlate.jsx
@@ -1,7 +1,7 @@
-import NamePlate1 from '../../assets/NamePlate1.png'
-import NamePlate2 from '../../assets/NamePlate2.png'
-import NamePlate3 from '../../assets/NamePlate3.png'
-import NamePlate4 from '../../assets/NamePlate4.png'
+import plate1 from '../../assets/plate1.png'
+import plate2 from '../../assets/plate2.png'
+import plate3 from '../../assets/plate3.png'
+import plate4 from '../../assets/plate4.png'
 import Container from '../Layouts/Container'
 
 
@@ -10,10 +10,10 @@ const NamePlate = () => {
         <div className='py-[50px] md:py-[117px]'>
             <Container>
                 <div className="md:flex justify-between">
-                    <img src={NamePlate1} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
-                    <img src={NamePlate2} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
-                    <img src={NamePlate3} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
-                    <img src={NamePlate4} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
+                    <img src={plate1} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
+                    <img src={plate2} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
+                    <img src={plate3} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
+                    <img src={plate4} alt="" className='w-[60%] mx-auto md:w-[100%]'/>
                 </div>
             </Container>
         </div>
